c236475b0f6d8c3acc2313b22b59fbef4697432b
Roman Shidlovsky <jagerlion@mail.ru>
2022-09-27 11:05:01 +0300

    Добавьте файлы проекта.

---
+++ Faker.Core/Interfaces/IFaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core.Interfaces
+{
+    public interface IFaker
+    {
+        T Create<T>();
+        object Create(Type t);
+    }
+}
